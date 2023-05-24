7969d4017240b83e7076396c98d22c194e254ea8
MateuszPisula01 <21051375-MateuszPisula01@users.noreply.replit.com>
2023-05-24 13:09:46 +0000

    Zrobione

--- Main.java
+++ Main.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
 
+int x, wynik;
+  
+    Scanner in= new Scanner(System.in);
     
-    System.out.println("Hello world!");
-  }
+    System.out.println("Podaj liczbe");
+     x = in.nextInt();
+
+    wynik = 0;
+
+    while (x != 0) {
+
+      wynik += x%10;
+
+      x /= 10;
+    }
+System.out.println("suma cyfr podanej liczby wynosi " + wynik);
+}
+ 
 }
